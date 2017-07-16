51b7e8e131d703a1dcdf8b1ae52af67125924ef1
Walter <jnunes.walter@gmail.com>
2017-07-16 15:42:29 -0300

    concat different pages in index

--- catalog.py
+++ catalog.py
@@ -29,9 +29,21 @@ def detailCategory(catalog_id):
 	category = session.query(Category).filter_by(id = catalog_id).first()
 	categoryName = category.name
 	categoryItems = session.query(CatalogItem).filter_by(category_id = catalog_id).all()
+	test = 1;
+	return render_template('index.html', categories = categories, categoryItems = categoryItems, categoryName = categoryName, test = test)
 
-	return render_template('categories.html', categories = categories, categoryItems = categoryItems, categoryName = categoryName)
+@app.route('/catalog2/<int:catalog_id>')
+@app.route('/catalog2/<int:catalog_id>/items')
+def detailCategory2(catalog_id):
+	# Get all categories
+	categories = session.query(Category).all()
 
+
+	category = session.query(Category).filter_by(id = catalog_id).first()
+	categoryName = category.name
+	categoryItems = session.query(CatalogItem).filter_by(category_id = catalog_id).all()
+	test2 = 1;
+	return render_template('index.html', categories = categories, categoryItems = categoryItems, categoryName = categoryName, test2 = test2)
 
 
 if __name__ == '__main__':
